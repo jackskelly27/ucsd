68b8d5ca6c21407ceb0b7f76099964767852111f
Matthew Kelly <matthewkelly@Matthews-iMac.local>
2022-05-16 10:31:04 -0500

    initial

--- backend/models/activityModel.js
+++ backend/models/activityModel.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const activitySchema = mongoose.Schema({
-    name: { type: String, required: true},
+    name: String,
     type: String,
     description: String,
     neighborhood: String,
