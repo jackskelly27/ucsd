167804534639d98f9cd9fc8dd0c4d381f7bf3b2e
Jacks Kelly <jacks.kelly@gmail.com>
2022-05-29 17:22:12 -0500

    Recent changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,37 +20,3 @@ function App() {
 }
 
 export default App;
-
-// const API_URL = '/api/goals/'
-
-// class App extends React.Component {
-
-//   state = {
-//     activities: []
-//   }
-  
-//   getActivities = () => {
-//     axios.get(API_URL)
-//       .then((response) => {
-//         const data = response.data;
-//         this.setState({ activities: data})
-//       })
-//       .catch(() => {
-//         console.log("Error");
-//       });
-//   };
-
-//   componentDidMount = () => {
-//     this.getActivities();
-//   }
-
-//   render() {
-//     return(
-//       <div>
-//         <h1>Welcome to my App</h1>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
