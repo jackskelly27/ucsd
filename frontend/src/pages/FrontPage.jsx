89bd069f27cc9fae3c04bbcb4f926e436e548b38
Jacks Kelly <jacks.kelly@gmail.com>
2022-05-31 19:51:27 -0500

    Add paragraph test

--- frontend/src/pages/FrontPage.jsx
+++ frontend/src/pages/FrontPage.jsx
@@ -4,11 +4,14 @@ import '../App.css'
 
 function FrontPage() {
   return (
+    <>
+    <p>This is a paragraph.</p>
     <div class="container">
       <Link to="/activities" className="btn link">Activities</Link>
       <Link to="/resources" className="btn link">Digital Resources</Link>
       <Link to="/gyms" className="btn link">Gyms</Link>
     </div>
+    </>
   )
 }
 
