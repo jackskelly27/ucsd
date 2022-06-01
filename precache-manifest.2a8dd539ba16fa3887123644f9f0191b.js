eedcb9d75828d41c2246dcc5d4492d266ff5ad84
jackskelly27 <jackskelly27@users.noreply.github.com>
2022-06-01 00:04:06 +0000

    deploy: cc75019d97f0d8f47bc23c023ce25ee8c4c13b5f

---
+++ precache-manifest.2a8dd539ba16fa3887123644f9f0191b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8e7f57639b6113aea5b",
+    "url": "/ucsd/static/css/main.06a93615.chunk.css"
+  },
+  {
+    "revision": "a8e7f57639b6113aea5b",
+    "url": "/ucsd/static/js/main.a8e7f576.chunk.js"
+  },
+  {
+    "revision": "2e8f1a6b9f28a667d85d",
+    "url": "/ucsd/static/js/1.2e8f1a6b.chunk.js"
+  },
+  {
+    "revision": "8916a0461164cbd71320",
+    "url": "/ucsd/static/js/2.8916a046.chunk.js"
+  },
+  {
+    "revision": "6a1d8ccf3d29718ce877",
+    "url": "/ucsd/static/js/runtime~main.6a1d8ccf.js"
+  },
+  {
+    "revision": "6b33f0e4aad74649c731a8ef81a71b41",
+    "url": "/ucsd/index.html"
+  }
+];
