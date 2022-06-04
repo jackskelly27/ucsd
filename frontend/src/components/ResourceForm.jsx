851c976724796f21e65656bfc3ad399ecde9b02a
Jacks Kelly <jacks.kelly@gmail.com>
2022-06-04 16:43:03 -0500

    deploy

--- frontend/src/components/ResourceForm.jsx
+++ frontend/src/components/ResourceForm.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {useForm} from 'react-hook-form'
 import {useNavigate} from 'react-router-dom'
 
+// fix deploy, remove soon
 function ResourceForm() {
     const navigate = useNavigate();
     const { register, handleSubmit } = useForm();
