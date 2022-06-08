5abfc95d1933d1fabfae4f3b75ab8bdef52675c8
Jacks Kelly <jacks.kelly@gmail.com>
2022-06-08 09:41:20 -0500

    Redeploy with header logos

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 function Header() {
   return (
     <div>
-      <img src="../../images/imgage.png" alt="UCSD Logo" />
-      <h1>Health Resources Near You</h1>
+      <img class = "img-left" src="/image.png" alt="UCSD Logo" />
+      <img class = "img-right" src="/image (1).png" alt="EIM Logo" />
+      <h1 class= "logoheader">Health Resources Near You</h1>
     </div>
   )
 }
