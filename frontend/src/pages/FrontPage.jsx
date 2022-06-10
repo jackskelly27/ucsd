e6a322e79cb28e8919557e168f03d435ba9a3576
Jacks Kelly <jacks.kelly@gmail.com>
2022-06-09 23:07:40 -0500

    Added more pictures to front page with descriptions, changed some CSS

--- frontend/src/pages/FrontPage.jsx
+++ frontend/src/pages/FrontPage.jsx
@@ -11,17 +11,20 @@ function FrontPage() {
     <div className="container">
       <div className="card">
         <h3>Physical Activities</h3>
-        <img src="ActivitiesImage.png" alt="Physical Activities" height="110"/>
+        <img src="ActivitiesImage.png" alt="Physical Activities" height="100"/>
+        <span className="k6">Explore the locations to walk, run, bike, swim, hike, dance, play sports, do yoga, etc. around San Diego!</span>
         <ActivityForm />
       </div>
       <div className="card">
         <h3>Digital Resources</h3>
-        <img src="ResourcesImage.png" alt="Digital Resources" height="110"/>
+        <img src="ResourcesImage.png" alt="Digital Resources" height="120"/>
+        <span className="k6">Discover YouTube channels, fitness apps, and cable channels to assist with exercise on your own time and in your own space.</span>
         <ResourceForm />
       </div>
       <div className="card">
         <h3>Gyms</h3>
         <img src="GymsImage.png" alt="Gyms" height="110"/>
+        <span className="k6">Search gyms and community centers around town to find a convenient location near you.</span>
         <GymForm />
       </div>
     </div>
