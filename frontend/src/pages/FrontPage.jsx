54d9d102da5c19c033ef0d1104d274a1a1be0500
Jacks Kelly <jacks.kelly@gmail.com>
2022-06-11 12:22:21 -0500

    Removed carousel, will add new one

--- frontend/src/pages/FrontPage.jsx
+++ frontend/src/pages/FrontPage.jsx
@@ -3,18 +3,10 @@ import '../App.css'
 import ActivityForm from '../components/ActivityForm';
 import ResourceForm from '../components/ResourceForm';
 import GymForm from '../components/GymForm';
-import Carousel, {CarouselItem} from "../components/Carousel";
 
 function FrontPage() {
   return (
     <>
-    <Carousel>
-      <CarouselItem><img src="RotatingImage1.png" alt="RotatingImage1" height="350px"/></CarouselItem>
-      <CarouselItem><img src="RotatingImage2.png" alt="RotatingImage2" height="350px"/></CarouselItem>
-      <CarouselItem><img src="RotatingImage3.png" alt="RotatingImage3" height="350px"/></CarouselItem>
-      <CarouselItem><img src="RotatingImage4.png" alt="RotatingImage4" height="350px"/></CarouselItem>
-      <CarouselItem><img src="RotatingImage5.png" alt="RotatingImage5" height="350px"/></CarouselItem>
-    </Carousel>
     <span className="k5">Please enjoy the free to low-cost resources collected below. Remember, you live in San Diego! Find your favorite local coastline, park, or urban space to get out and move your body. If you're 65+, <a href="http://tools.silversneakers.com" target="_blank" rel="noopener noreferrer" class="emphasized">you may be eligible for a free program, Silver Sneakers, click here to find out</a>.</span>
     <div className="container">
       <div className="card">
