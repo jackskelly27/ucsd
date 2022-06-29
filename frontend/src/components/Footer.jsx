6423c7c8aef4b4984a2db657bd191a26ca9089e0
Jacks Kelly <jacks.kelly@gmail.com>
2022-06-28 22:51:33 -0500

    Added content

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,11 +6,11 @@ const today = new Date();
 function Footer() {
   return (
     <div className="footer">
-      <h3>Connect with us!</h3>
+      <h2>Connect with us!</h2>
       <div className="container5">
-      <a href="https://www.facebook.com/UCSDPrimaryCareFamilyMedicineLaJolla" alt="Facebook" class="fa fa-facebook"> </a>
-      <a href="https://twitter.com/UCSDHealth?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" alt="Twitter" class="fa fa-twitter"> </a>
-      <a href="https://www.instagram.com/ucsdhealth/" alt="Twitter" class="fa fa-instagram"> </a>
+      <a href="https://www.facebook.com/UCSDPrimaryCareFamilyMedicineLaJolla" alt="Facebook" class="fa fa-facebook" id="click" target="_blank" rel="noopener noreferrer"> </a>
+      <a href="https://twitter.com/UCSDHealth?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" alt="Twitter" class="fa fa-twitter" id="click" target="_blank" rel="noopener noreferrer"> </a>
+      <a href="https://www.instagram.com/ucsdhealth/" alt="Twitter" class="fa fa-instagram" id="click" target="_blank" rel="noopener noreferrer"> </a>
       </div>
       <h6>Matthew Kelly &copy; {today.getFullYear()}</h6>
     </div>
